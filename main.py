ed0dd55b1cc18b919ed226bb48715f713849283b
SkBlaz <skrljblaz@gmail.com>
2017-08-14 16:28:57 +0200

    Minimal examples

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 ## this is the main analysis class
 ## this set of scripts generates statistics from the SBML database!
+
+
+
+## do for each model, collect compartments, reactions, events, species..
+## simply plot in the first step!
